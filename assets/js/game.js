99c70e2707718339c4d4d822e0a7edbe63babf59
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-08-15 00:30:41 -0500

    add high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,7 +28,7 @@ var fightOrSkip = function () {
 
 var fight = function (enemy) {
 
-  var isPlayerturn = true;
+  var isPlayerTurn = true;
 
   if (Math.random() > 0.5) {
     isPlayerTurn = false;
@@ -147,11 +147,20 @@ var startGame = function () {
 var endGame = function () {
   window.alert("The game has now ended. Let's see how you did!");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-  } else {
-    window.alert("You've lost your robot in battle.");
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player have more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
